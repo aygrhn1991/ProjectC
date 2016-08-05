649362918f212cd4ec18295bffbc72a7937c58ce
chenyufeng <411610370@qq.com>
2016-08-05 18:36:22 +0800

    123

---
+++ ProjectC/ProjectC/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Check(string data)
+        {
+            if (data == "ookk")
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetD(string user1,string password1)
+        {
+            return Json("getit",JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ProjectC/ProjectC/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../extentions/javascript/messages_zh.min.js" />
+/// <reference path="../extentions/javascript/projectc.js" />
+/// <reference path="bootstrap.min.js" />
+/// <reference path="jquery.form.min.js" />
+/// <reference path="jquery.validate.min.js" />
+/// <reference path="jquery-2.2.4.js" />
